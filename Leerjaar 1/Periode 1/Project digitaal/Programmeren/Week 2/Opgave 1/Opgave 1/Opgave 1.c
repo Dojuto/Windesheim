ced85c0f8d23e1c3502491150bc37066f438b192
Dojuto <drost.sem@gmail.com>
2024-09-11 10:30:01 +0200

    Opgave 1

--- Leerjaar 1/Periode 1/Project digitaal/Programmeren/Week 2/Opgave 1/Opgave 1/Opgave 1.c
+++ Leerjaar 1/Periode 1/Project digitaal/Programmeren/Week 2/Opgave 1/Opgave 1/Opgave 1.c
@@ -1,4 +1,32 @@
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
 void main()
 {
-	printf("hoi")
+	int positief1;
+	int positief2;
+	int antwoord;
+	int overflow;
+
+	printf("Vul hier een integer in:\n");
+	scanf("%d", &positief1);
+
+	printf("Vul hier een tweede integer in:\n");
+	scanf("%d", &positief2);
+
+	antwoord = positief1 * positief2;
+	overflow = 21478364;
+
+	if (antwoord < overflow) {
+		printf("dit zijn de twee integers keer elkaar: %d\n", antwoord);
+	}
+	else if (antwoord > overflow) {
+		printf("overflow");
+	}
+
+	
+
+
+
 }
