f6503801f3365fc78fa5e974a405d3a8dcd54bef
Dojuto <drost.sem@gmail.com>
2024-09-11 16:38:41 +0200

    opgavens

--- Leerjaar 1/Periode 1/Project digitaal/Programmeren/Week 2/Opgave 5/Opgave 5/Opgave 5.c
+++ Leerjaar 1/Periode 1/Project digitaal/Programmeren/Week 2/Opgave 5/Opgave 5/Opgave 5.c
@@ -5,9 +5,22 @@
 void main()
 {
 	int N;
+	int i;
+	int sum = 0;
 	printf("Vul hier een cijfer in:\n");
 	scanf("%d", &N);
-	for
+	for (i = 1; i <= N; i++) {
+		sum += i;
+		if (i == N) {
+			printf("%d = %d", i, sum);
+		}
+		else {
+			printf("%d + ", i);
+		}
+	}
+   
+
+    
 
 
 
